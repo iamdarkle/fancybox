346d3eec243fbde811f1d8107771dc7d71388578
Ian Morland <ian@morland.me>
2021-12-13 02:02:28 +0000

    Upgrade to fancybox v4

---
+++ js/src/forum/index.js
@@ -0,0 +1,20 @@
+import app from 'flarum/forum/app';
+import { extend } from 'flarum/common/extend';
+import CommentPost from 'flarum/forum/components/CommentPost';
+import { Fancybox } from '@fancyapps/ui';
+
+app.initializers.add('darkle/fancybox', () => {
+  Fancybox.defaults.Image = { zoom: false };
+
+  extend(CommentPost.prototype, 'oncreate', function (vnode) {
+    const self = this;
+
+    this.element.querySelectorAll('img:not(.emoji):not(.Avatar):not(.PostMeta-ip img)').forEach((node) => {
+      const fancyboxEl = document.createElement('a');
+      fancyboxEl.setAttribute('data-fancybox', 'responsive');
+      fancyboxEl.setAttribute('data-src', node.getAttribute('data-src') || node.getAttribute('src'));
+
+      $(node).wrap(fancyboxEl);
+    });
+  });
+});
