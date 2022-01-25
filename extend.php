43cdaec46305b9198f468566c1dd2981c9ca3617
Darkle <darkle@protonmail.com>
2022-01-25 20:02:46 +0100

    Update extend.php

--- extend.php
+++ extend.php
@@ -3,7 +3,7 @@
 /*
  * This file is part of iamdarkle/fancybox
  *
- * Copyright (c) 2021 Tomás Romero.
+ * Copyright (c) 2022 Tomás Romero.
  *
  * For the full copyright and license information, please view the LICENSE.md
  * file that was distributed with this source code.
