27444976004998b39119ddd24a9bac7a8b1dd12c
Darkle <tomasromero@outlook.com>
2022-06-20 14:27:28 +0200

    Flamoji support & bump deps

--- js/src/forum/index.js
+++ js/src/forum/index.js
@@ -7,12 +7,14 @@ app.initializers.add('darkle/fancybox', () => {
   Fancybox.defaults.Image = { zoom: false };
 
   extend(CommentPost.prototype, 'oncreate', function (vnode) {
-    this.element.querySelectorAll('img:not(.emoji):not(.Avatar):not(.PostMeta-ip img):not([data-reaction]):not([data-link-preview])').forEach((node) => {
-      const fancyboxEl = document.createElement('a');
-      fancyboxEl.setAttribute('data-fancybox', 'responsive');
-      fancyboxEl.setAttribute('data-src', node.getAttribute('data-src') || node.getAttribute('src'));
+    this.element
+      .querySelectorAll('img:not(.emoji):not(.Avatar):not(.PostMeta-ip img):not([data-reaction]):not([data-link-preview]):not(.flamoji img)')
+      .forEach((node) => {
+        const fancyboxEl = document.createElement('a');
+        fancyboxEl.setAttribute('data-fancybox', 'responsive');
+        fancyboxEl.setAttribute('data-src', node.getAttribute('data-src') || node.getAttribute('src'));
 
-      $(node).wrap(fancyboxEl);
-    });
+        $(node).wrap(fancyboxEl);
+      });
   });
 });
