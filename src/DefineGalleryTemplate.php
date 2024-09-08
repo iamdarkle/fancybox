c633c23873fa82ef8b5e3cbab5f7f6ed958e1955
Darkle <tomasromero@outlook.com>
2024-09-08 04:35:06 +0200

    Update DefineGalleryTemplate.php

--- src/DefineGalleryTemplate.php
+++ src/DefineGalleryTemplate.php
@@ -8,15 +8,19 @@ class DefineGalleryTemplate
 {
     public function __invoke(Configurator $config)
     {
+        // Define the IMG-GALLERY tag template
         $tag = $config->tags->add('IMG-GALLERY');
         $tag->template = <<<XML
         <div class="f-carousel" data-fancybox="gallery">
-            <div class="f-carousel__track">
-                <xsl:apply-templates/>
+            <div class="f-carousel__viewport">
+                <div class="f-carousel__track">
+                    <xsl:apply-templates/>
+                </div>
             </div>
         </div>
         XML;
 
+        // Define the IMG-GALLERY-ITEM tag template
         $tag = $config->tags->add('IMG-GALLERY-ITEM');
         $tag->template = <<<XML
             <div class="f-carousel__slide">
