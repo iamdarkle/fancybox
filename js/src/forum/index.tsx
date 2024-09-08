9ec5ca53d429ec8b005017c5d8d061aa54cb3430
Darkle <tomasromero@outlook.com>
2024-09-08 05:38:57 +0200

    tsx

--- js/src/forum/index.tsx
+++ js/src/forum/index.tsx
@@ -7,30 +7,34 @@ import '@fancyapps/ui/dist/fancybox/fancybox.css';
 import '@fancyapps/ui/dist/carousel/carousel.css';
 
 app.initializers.add('darkle/fancybox', () => {
-  extend(CommentPost.prototype, 'oncreate', function (vnode) {
+  extend(CommentPost.prototype, 'oncreate', function (this: any, vnode: any) {
+    console.log('Running oncreate for CommentPost');
+
     // Wrap images with anchor tags for Fancybox, only if not already wrapped
     this.element
-      .querySelectorAll('.Post-body img:not(.emoji):not(.Avatar):not(.PostMeta-ip img):not([data-reaction]):not([data-link-preview]):not(.flamoji img):not(.countryFlag):not(.no-fancybox)')
+      .querySelectorAll<HTMLImageElement>('.Post-body img:not(.emoji):not(.Avatar):not(.PostMeta-ip img):not([data-reaction]):not([data-link-preview]):not(.flamoji img):not(.countryFlag):not(.no-fancybox)')
       .forEach((node) => {
         if (!node.closest('a[data-fancybox="gallery"]')) {
           const src = node.getAttribute('data-src') || node.getAttribute('src');
           const fancyboxEl = document.createElement('a');
           fancyboxEl.setAttribute('data-fancybox', 'gallery');
-          fancyboxEl.href = src;
-          node.parentNode.insertBefore(fancyboxEl, node);
+          fancyboxEl.href = src!;
+          node.parentNode!.insertBefore(fancyboxEl, node);
           fancyboxEl.appendChild(node);
         }
       });
 
+    console.log('Initializing Carousel');
     // Initialize Carousel
     this.element
-      .querySelectorAll('.f-carousel')
+      .querySelectorAll<HTMLElement>('.f-carousel')
       .forEach((carousel) => {
         new Carousel(carousel, {
           Dots: false,
         });
       });
 
+    console.log('Initializing Fancybox');
     // Initialize Fancybox
     Fancybox.bind('[data-fancybox="gallery"]', {
       Carousel: {
