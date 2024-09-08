6a0c396ee75841ea956b62de99a4685509e429e0
Darkle <tomasromero@outlook.com>
2024-09-08 05:33:59 +0200

    try

--- js/src/forum/index.js
+++ js/src/forum/index.js
@@ -3,19 +3,23 @@ import { extend } from 'flarum/common/extend';
 import CommentPost from 'flarum/forum/components/CommentPost';
 import { Fancybox } from '@fancyapps/ui';
 import { Carousel } from '@fancyapps/ui';
+import '@fancyapps/ui/dist/fancybox/fancybox.css';
+import '@fancyapps/ui/dist/carousel/carousel.css';
 
 app.initializers.add('darkle/fancybox', () => {
   extend(CommentPost.prototype, 'oncreate', function (vnode) {
-    // Wrap images with anchor tags for Fancybox
+    // Wrap images with anchor tags for Fancybox, only if not already wrapped
     this.element
       .querySelectorAll('.Post-body img:not(.emoji):not(.Avatar):not(.PostMeta-ip img):not([data-reaction]):not([data-link-preview]):not(.flamoji img):not(.countryFlag):not(.no-fancybox)')
       .forEach((node) => {
-        const src = node.getAttribute('data-src') || node.getAttribute('src');
-        const fancyboxEl = document.createElement('a');
-        fancyboxEl.setAttribute('data-fancybox', 'gallery');
-        fancyboxEl.href = src;
-        node.parentNode.insertBefore(fancyboxEl, node);
-        fancyboxEl.appendChild(node);
+        if (!node.closest('a[data-fancybox="gallery"]')) {
+          const src = node.getAttribute('data-src') || node.getAttribute('src');
+          const fancyboxEl = document.createElement('a');
+          fancyboxEl.setAttribute('data-fancybox', 'gallery');
+          fancyboxEl.href = src;
+          node.parentNode.insertBefore(fancyboxEl, node);
+          fancyboxEl.appendChild(node);
+        }
       });
 
     // Initialize Carousel
