6a3a45ba12b471714606148b68665e92460e867e
Darkle <tomasromero@outlook.com>
2024-09-08 15:12:34 +0200

    try

--- js/src/forum/index.tsx
+++ js/src/forum/index.tsx
@@ -1,15 +1,28 @@
 import app from 'flarum/forum/app';
 import { extend } from 'flarum/common/extend';
 import CommentPost from 'flarum/forum/components/CommentPost';
-import { Fancybox } from '@fancyapps/ui';
+import { Fancybox, Carousel } from '@fancyapps/ui';
 import '@fancyapps/ui/dist/fancybox/fancybox.css';
+import '@fancyapps/ui/dist/carousel/carousel.css';
 
 app.initializers.add('darkle/fancybox', () => {
   extend(CommentPost.prototype, 'oncreate', function () {
+    console.log('Initializing Carousel');
+
+    // Initialize Carousel for each gallery
+    this.element.querySelectorAll('.f-carousel').forEach((carouselElement) => {
+      new Carousel(carouselElement, {
+        // Carousel options
+        Dots: false,
+        infinite: true,
+      });
+    });
+
     console.log('Initializing Fancybox');
-    // Initialize Fancybox for both single images and galleries
+
+    // Initialize Fancybox for all elements with data-fancybox attribute
     Fancybox.bind('[data-fancybox]', {
-      // Add any custom options here
+      // Fancybox options can be added here
     });
   });
 });
--- src/WrapImagesInGallery.php
+++ src/WrapImagesInGallery.php
@@ -6,21 +6,15 @@ use s9e\TextFormatter\Renderer;
 
 class WrapImagesInGallery
 {
-    const MATCH_IMG_TAGS = '((?:<UPL-IMAGE-PREVIEW[^>]*>(?:(?!<\/UPL-IMAGE-PREVIEW>).)*<\/UPL-IMAGE-PREVIEW>)|(?:<IMG[^>]*>(?:(?!<\/IMG>).)*<\/IMG>))';
+    const MATCH_IMG_TAGS = '((?:<IMG[^>]*>(?:(?!<\/IMG>).)*<\/IMG>))';
     const MATCH_GALLERY_REGEX = '((?:'.self::MATCH_IMG_TAGS.'(?:\n|<br\/>)*){2,})';
 
     public function __invoke(Renderer $renderer, $context, string $xml): string
     {
-        // Wrap gallery images
-        $xml = preg_replace_callback('/'.self::MATCH_GALLERY_REGEX.'/m', function ($matches) {
+        return preg_replace_callback('/'.self::MATCH_GALLERY_REGEX.'/m', function ($matches) {
             $m = preg_replace('/'.self::MATCH_IMG_TAGS.'/m', '<IMG-GALLERY-ITEM>$1</IMG-GALLERY-ITEM>', $matches[0]);
 
             return '<IMG-GALLERY>' . str_replace('<br/>', '', $m) . '</IMG-GALLERY>';
         }, $xml);
-
-        // Wrap single images
-        $xml = preg_replace('/<IMG src="([^"]+)"(.*?)>(.*?)<\/IMG>/', '<a href="$1" data-fancybox="single"><img src="$1" $2>$3</img></a>', $xml);
-
-        return $xml;
     }
 }
