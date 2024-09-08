065a7568ef3c061ff18045978d75d6586e6b0038
Darkle <tomasromero@outlook.com>
2024-09-08 15:23:26 +0200

    try

--- js/src/forum/index.js
+++ js/src/forum/index.js
@@ -1,9 +1,12 @@
 import app from 'flarum/forum/app';
 import { extend } from 'flarum/common/extend';
 import CommentPost from 'flarum/forum/components/CommentPost';
-import { Fancybox, Carousel } from '@fancyapps/ui';
-import '@fancyapps/ui/dist/fancybox/fancybox.css';
+
+import { Carousel } from '@fancyapps/ui/dist/carousel/carousel.esm.js';
 import '@fancyapps/ui/dist/carousel/carousel.css';
+
+import { Fancybox } from '@fancyapps/ui/dist/fancybox/fancybox.esm.js';
+import '@fancyapps/ui/dist/fancybox/fancybox.css';
 
 app.initializers.add('darkle/fancybox', () => {
   extend(CommentPost.prototype, 'oncreate', function () {
@@ -21,7 +24,7 @@ app.initializers.add('darkle/fancybox', () => {
     console.log('Initializing Fancybox');
 
     // Initialize Fancybox for all elements with data-fancybox attribute
-    Fancybox.bind('[data-fancybox]', {
+    Fancybox.bind('[data-fancybox="gallery"]', {
       // Fancybox options can be added here
     });
   });
