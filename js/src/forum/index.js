8ccf28d9e87a850c05d5fa0138108b9c288f4bd5
Darkle <tomasromero@outlook.com>
2024-09-08 18:14:43 +0200

    try

--- js/src/forum/index.js
+++ js/src/forum/index.js
@@ -1,13 +1,11 @@
 import app from 'flarum/forum/app';
 import { extend } from 'flarum/common/extend';
 import CommentPost from 'flarum/forum/components/CommentPost';
-import { Fancybox, Carousel } from '@fancyapps/ui';
+import { Fancybox } from '@fancyapps/ui';
 import '@fancyapps/ui/dist/fancybox/fancybox.css';
-import '@fancyapps/ui/dist/carousel/carousel.css';
 
 app.initializers.add('darkle/fancybox', () => {
   extend(CommentPost.prototype, 'oncreate', function () {
-    // Select all images that need to be processed
     const images = this.element.querySelectorAll(
       '.Post-body img:not(.emoji):not(.Avatar):not(.PostMeta-ip img):not([data-reaction]):not([data-link-preview]):not(.flamoji img):not(.countryFlag):not(.no-fancybox)'
     );
@@ -16,32 +14,20 @@ app.initializers.add('darkle/fancybox', () => {
 
     // Create a container for the gallery if there are multiple images
     const isGallery = images.length > 1;
-    const galleryContainer = isGallery ? document.createElement('div') : null;
-    
-    if (isGallery) {
-      galleryContainer.setAttribute('data-fancybox', 'gallery');
-      this.element.appendChild(galleryContainer);
-    }
 
     images.forEach((node) => {
       const src = node.getAttribute('data-src') || node.getAttribute('src');
       const fancyboxEl = document.createElement('a');
       fancyboxEl.href = src;
-      
-      if (isGallery) {
-        // Append image to gallery container
-        galleryContainer.appendChild(fancyboxEl);
-      } else {
-        // Treat as a single image
-        fancyboxEl.setAttribute('data-fancybox', 'single');
-        node.parentNode.insertBefore(fancyboxEl, node);
-      }
+      fancyboxEl.setAttribute('data-fancybox', isGallery ? 'gallery' : 'single');
 
+      // Insert the anchor element before the image and append the image to it
+      node.parentNode.insertBefore(fancyboxEl, node);
       fancyboxEl.appendChild(node);
     });
 
-    // Bind Fancybox based on the context
-    Fancybox.bind('[data-fancybox="gallery"]', {
+    // Bind Fancybox to the container
+    Fancybox.bind(this.element, '[data-fancybox]', {
       Carousel: {
         infinite: false,
       },
@@ -60,11 +46,11 @@ app.initializers.add('darkle/fancybox', () => {
         zoom: false,
       },
     });
+  });
 
-    Fancybox.bind('[data-fancybox="single"]', {
-      Images: {
-        zoom: false,
-      },
-    });
+  extend(CommentPost.prototype, 'onremove', function () {
+    // Unbind Fancybox when the component is removed
+    Fancybox.unbind(this.element);
+    Fancybox.close();
   });
 });
--- src/DefineGalleryTemplate.php
+++ src/DefineGalleryTemplate.php
@@ -8,18 +8,40 @@ class DefineGalleryTemplate
 {
     public function __invoke(Configurator $config)
     {
+        // Define the gallery container
         $tag = $config->tags->add('IMG-GALLERY');
         $tag->template = <<<XML
-        <div class="fancybox-gallery" data-fancybox="gallery">
+        <div>
             <xsl:apply-templates/>
         </div>
         XML;
 
+        // Define each gallery item
         $tag = $config->tags->add('IMG-GALLERY-ITEM');
         $tag->template = <<<XML
-            <div class="fancybox-item">
-                <xsl:apply-templates/>
-            </div>
+            <a data-fancybox="gallery" href="{@src}">
+                <img src="{@src}" alt="{@alt}" loading="lazy"/>
+            </a>
         XML;
+
+        // Ensure each image is wrapped appropriately
+        foreach (['IMG', 'UPL-IMAGE-PREVIEW'] as $tagName) {
+            if ($config->tags->exists($tagName)) {
+                $tag = $config->tags->get($tagName);
+
+                // Wrap each image with a link for Fancybox
+                $dom = $tag->template->asDOM();
+                $imgs = $dom->getElementsByTagName('img');
+                foreach ($imgs as $img) {
+                    $img->setAttribute('loading', 'lazy'); // Adding the lazy loading attribute
+                    $a = $dom->createElement('a');
+                    $a->setAttribute('data-fancybox', 'gallery');
+                    $a->setAttribute('href', $img->getAttribute('src'));
+                    $img->parentNode->replaceChild($a, $img);
+                    $a->appendChild($img);
+                }
+                $dom->saveChanges();
+            }
+        }
     }
 }
