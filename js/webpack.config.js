c52e1ab58cb2ef42b68c97e905d4b7186e0633c8
Darkle <tomasromero@outlook.com>
2024-09-10 19:22:10 +0200

    style-loader css-loader webpack-merge

--- js/webpack.config.js
+++ js/webpack.config.js
@@ -1,1 +1,15 @@
-module.exports = require('flarum-webpack-config')();
+const config = require('flarum-webpack-config');
+const { merge } = require('webpack-merge');
+
+const customConfig = {
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader'],
+      },
+    ],
+  },
+};
+
+module.exports = merge(config(), customConfig);
